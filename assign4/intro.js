a3d1128c3644dcfae33a609a2cab176110d7dee9
Binoy Abraham .M <63875478+binoy2619@users.noreply.github.com>
2020-07-15 14:18:51 +0530

    console.log function

---
+++ assign4/intro.js
@@ -0,0 +1,18 @@
+console.log("to print in console") // to print something in 
+
+
+console.time("time taken")   // gives the time taken to execute below code
+
+console.error("this is error")   // throws error message
+console.warn("this is warning")  // throws warning message
+
+var a = {name:"brp",age:"44"}
+var b = {name:"p",age:"42244"}
+var c = {name:"gg",age:"4424"}
+console.log({a,b,c});
+console.table({a,b,c});           // prints in table format
+
+
+console.timeEnd("time taken")
+
+console.log("%c color effect","color:red");    // gives color effect
