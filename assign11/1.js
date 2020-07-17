3b7a36938dc4cd8092e6409fe15f81260f54f91a
Binoy Abraham .M <63875478+binoy2619@users.noreply.github.com>
2020-07-17 14:28:53 +0530

    to take into input from the user as a marks and grade him accordingly

---
+++ assign11/1.js
@@ -0,0 +1,23 @@
+ // Using if else
+ let marks = Number(prompt("Enter the marks"));
+
+ if(marks >= 95 && marks <= 100){
+     console.log(`marks are ${marks} and grade is A`);
+ }
+ else if(marks >= 90 && marks < 95 ){
+    console.log(`marks are ${marks} and grade is B`);
+ }
+ else if(marks >= 85 && marks < 90 ){
+    console.log(`marks are ${marks} and grade is C`);
+ }
+ else if(marks >= 80 && marks < 85 ){
+    console.log(`marks are ${marks} and grade is D`);
+ }
+ else {
+    console.log(`marks are ${marks} and grade is Fail`);
+ }
+
+
+
+
+ 
