37f929ca6d8d6c3addca8d74361f2d1afad2461e
Binoy Abraham .M <63875478+binoy2619@users.noreply.github.com>
2020-07-20 02:13:41 +0530

    Add files via upload

---
+++ day 5/assign12/1.js
@@ -0,0 +1,11 @@
+console.log("For loop");
+
+let a = "fizz ";
+let b = "buzz ";
+let c = "fizzbizz ";  
+for(let i = 0; i<100; i++){
+    console.log(a.repeat(3));
+    console.log(b.repeat(5));
+    console.log(c.repeat(3));
+    console.log(c.repeat(5));
+}  
