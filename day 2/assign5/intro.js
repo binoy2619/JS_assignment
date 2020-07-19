e3e5ca73c0afb5dc6344669e7d656b049744d076
Binoy Abraham .M <63875478+binoy2619@users.noreply.github.com>
2020-07-20 00:41:49 +0530

    Add files via upload

---
+++ day 2/assign5/intro.js
@@ -0,0 +1,32 @@
+// var
+/* var   =  it is for storing value or u can say it acts as a container
+            and it gloabal in nature.
+            values can be changed in var
+            it is function type*/
+var name = "letsupgrade is best";
+console.log(name);
+name = "letsupgrade is awesome";
+console.log(name);
+
+// let
+/* let = - declared in {} brackets
+         - it is block type
+         - it is local in natue which means scope of that variable which is declared - - in {} bracket */
+
+{
+    let name = "boom";
+    console.log(name);
+}
+
+// const
+/* const = - const variables are fixed which means the value present in the variable 
+             cannot be changed after assigning once */
+
+const kick = 12;
+console.log(kick);
+const array = ["apple","mangoe","orange"];
+console.log(array);
+array.push("grapes","banana");
+console.log(array);
+            
+        
