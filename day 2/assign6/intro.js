e9c1bbea58c95262b600728abe37a38f70ebde2f
Binoy Abraham .M <63875478+binoy2619@users.noreply.github.com>
2020-07-20 00:51:41 +0530

    Add files via upload

---
+++ day 2/assign6/intro.js
@@ -0,0 +1,26 @@
+// data types
+
+
+// string
+var name = "binoy";
+console.log(name);
+
+// boolean
+var n = true;
+console.log(n);
+
+// integer
+var g = 332;
+console.log(g);
+
+// array
+var array = [1,2,3,4];
+console.log(array);
+
+// undefined
+var a ;
+console.log(a);
+
+// NULL
+ var b = null;
+ console.log(b);
